c910ca32d4f225f387bb5fa6e742d42bcebc0ccd
PhanTrungKien102 <phantrungkiennd102@gmail.com>
2023-10-30 12:28:22 +0700

    create project and code UI

---
+++ Operating Bus/UserChooseTypeRoute.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Operating_Bus
+{
+    /// <summary>
+    /// Interaction logic for UserChooseTypeRoute.xaml
+    /// </summary>
+    public partial class UserChooseTypeRoute : Window
+    {
+        public UserChooseTypeRoute()
+        {
+            InitializeComponent();
+        }
+    }
+}
